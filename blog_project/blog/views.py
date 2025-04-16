afc187682c366e32bd7c3bfe20453987290ef69a
manuelp1601 <92879473+manuelp1601@users.noreply.github.com>
2025-04-16 19:49:35 -0400

    authentication function, and testing screens.

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.http import HttpResponse
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'test/registration.html', {'form': form})
+
+def home(request):
+    return render(request, 'test/home.html')
